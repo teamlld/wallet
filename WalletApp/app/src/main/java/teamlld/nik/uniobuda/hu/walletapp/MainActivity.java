8720302846d03d37abbfac3c560534811a9208cf
Atee134 <l.atee.1995@gmail.com>
2017-03-11 20:48:53 +0100

    nagyszerű szöveg?

--- WalletApp/app/src/main/java/teamlld/nik/uniobuda/hu/walletapp/MainActivity.java
+++ WalletApp/app/src/main/java/teamlld/nik/uniobuda/hu/walletapp/MainActivity.java
@@ -10,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //na ideírtam valamit
+
         // test line for git
     }
 }
