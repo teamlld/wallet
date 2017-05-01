359e8254f1d3e1371220768104a084e7567c273d
teamlld <lg.gergo@gmail.com>
2017-05-01 11:53:56 +0200

    navdrawer minta hozzáadva, filewriter class hozzáadva

---
+++ WalletApp/app/src/main/java/teamlld/nik/uniobuda/hu/walletapp/FileWriter.java
@@ -0,0 +1,36 @@
+package teamlld.nik.uniobuda.hu.walletapp;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by GERGO on 2017.05.01..
+ */
+
+public class FileWriter {
+
+
+    String baseDir = android.os.Environment.getExternalStorageDirectory().getAbsolutePath();
+    String fileName = "Transactions.csv";
+    String filePath = baseDir + File.separator + fileName;
+
+    public void WriteTextFileTest(List<Transaction> list)
+    {
+        File f = new File(filePath );
+
+        try {
+            FileOutputStream stream = new FileOutputStream(f);
+            for (int i=0;i<list.size();i++) {
+                stream.write(list.get(i).getName().getBytes());
+
+            }
+            stream.close();
+        }
+        catch (IOException e) {
+
+        }
+
+    }
+}
